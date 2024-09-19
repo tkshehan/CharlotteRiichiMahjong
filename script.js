db2e50cef6e73db6f6488d18bb55d18dc9547889
tkshehan <thomas.shehan88@gmail.com>
2024-09-19 13:31:18 -0400

    update schedule

--- script.js
+++ script.js
@@ -2,20 +2,17 @@ const sheetID = '1HPuYNytvYN-vVzfNxhzincWE5gCAGkepdA5GvO7Gmvw';
 const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
 const sheetName ='Participants';
 const sheetName2 = 'Leaderboard';
-const query = encodeURIComponent('select *');
+const query = encodeURIComponent('select B,C,D');
 const url = `${base}&sheet=${sheetName}&tq=${query}`;
 const url2 = `${base}&sheet=${sheetName2}&tq=${query}`;
 
+var partData;
+var leadData;
  
 fetch(url)
 .then(res => res.text())
 .then(rep => {
-    const jsData = JSON.parse(rep.substring(47).slice(0,-2));
+    const partData = JSON.parse(rep.substring(47).slice(0,-2));
     console.log(jsData);
-    const colz = [];
-    jsData.table.cols.forEach((heading)=>{
-        if(heading.label) {
-            colz.push(heading.label);
-        }
-    })
+    
 })
